efccf1732571b27653734f0427b4779887931ad6
Ivaylo Ninov <ivailomn216@gmail.com>
2022-09-18 17:57:53 +0300

    add first steps in coding

---
+++ Programming-Basics-With-C#/First-Steps-In-Coding/RectangleArea/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int area = int.Parse(Console.ReadLine());
+            Console.WriteLine(area * area);
+        }
+    }
+}
