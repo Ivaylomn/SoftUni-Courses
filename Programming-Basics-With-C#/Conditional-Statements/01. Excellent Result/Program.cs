ba7b7e09c8938fb6437d8f66f29727a5616b18ce
Ivaylo Ninov <ivailomn216@gmail.com>
2022-09-19 11:13:20 +0300

    Add Conditional-Statements

---
+++ Programming-Basics-With-C#/Conditional-Statements/01. Excellent Result/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01._Excellent_Result
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double value = double.Parse(Console.ReadLine());
+
+            if (value >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
