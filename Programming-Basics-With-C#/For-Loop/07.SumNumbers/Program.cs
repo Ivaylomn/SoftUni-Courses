4c0efec6113657ad664cec203a2d2a7007c89b6e
Ivaylo Ninov <ivailomn216@gmail.com>
2022-09-23 11:03:37 +0300

    Add For-Loop

---
+++ Programming-Basics-With-C#/For-Loop/07.SumNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum = sum + number;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Programming-Basics-With-C#/For-Loop/10.OddEvenSum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _10OddEvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int evenNumber = 0;
+            int oddNumber = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    evenNumber += number;
+                }
+                else
+                {
+                    oddNumber += number;
+                }
+            }
+
+            if (oddNumber == evenNumber)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {evenNumber}");
+            }
+            else
+            {
+                double differnt = evenNumber - oddNumber;
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(differnt)}");
+            }
+        }
+    }
+}
