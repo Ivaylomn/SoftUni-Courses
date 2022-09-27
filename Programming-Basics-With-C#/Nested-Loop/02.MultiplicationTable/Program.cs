b7f64805d94c63eade8ccb938bc2c38f5b952a6e
Ivaylo Ninov <ivailomn216@gmail.com>
2022-09-27 11:09:09 +0300

    Add Nested-Loop

---
+++ Programming-Basics-With-C#/Nested-Loop/02.MultiplicationTable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int numOne = 1; numOne <= 10; numOne++)
+            {
+                for (int numTwo = 1; numTwo <= 10; numTwo++)
+                {
+                    Console.WriteLine($"{numOne} * {numTwo} = {numOne * numTwo}");
+                }
+            }
+        }
+    }
+}
---
+++ Programming-Basics-With-C#/Nested-Loop/06.Building/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _06Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int floorCount = int.Parse(Console.ReadLine());
+            int roomCount = int.Parse(Console.ReadLine());
+
+            for (int floors = floorCount; floors >= 1; floors--)
+            {
+                for (int rooms = 0; rooms < roomCount; rooms++)
+                {
+                    if (floors == floorCount)
+                    {
+                        Console.Write($"L{floors}{rooms} ");
+
+                        continue;
+                    }
+
+                    if (floors % 2 == 0)
+                    {
+                        Console.Write($"O{floors}{rooms} ");
+                    }
+                    else
+                    {
+                        Console.Write($"A{floors}{rooms} ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
