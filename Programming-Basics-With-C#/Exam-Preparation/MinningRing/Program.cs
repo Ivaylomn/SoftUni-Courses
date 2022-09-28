a2c3ac1bca38bd96a7bc5901dd843b05a345de91
Ivaylo Ninov <ivailomn216@gmail.com>
2022-09-28 10:30:59 +0300

    Add Exam-Preparation

---
+++ Programming-Basics-With-C#/Exam-Preparation/MinningRing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MinningRing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int videoCard = int.Parse(Console.ReadLine()) * 13;
+            int adapter = int.Parse(Console.ReadLine()) * 13;
+            double electricity = double.Parse(Console.ReadLine());
+            double income = (double.Parse(Console.ReadLine()) - electricity) * 13;
+
+            double costs = videoCard + adapter + 1000;
+            double days = Math.Ceiling(costs / income);
+
+            Console.WriteLine(costs);
+            Console.WriteLine(days);
+        }
+    }
+}
---
+++ Programming-Basics-With-C#/Exam-Preparation/SpaceShip/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SpaceShip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double lenght = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double averageHeightAstronauts = double.Parse(Console.ReadLine());
+
+            double rocket = width * lenght * height;
+            double rooms = (averageHeightAstronauts + 0.4) * 2 * 2;
+            double availableRooms = Math.Floor(rocket / rooms);
+
+            if (availableRooms > 10)
+            {
+                Console.WriteLine("The spacecraft is too big.");
+            }
+            else if (availableRooms >= 3 && availableRooms <= 10)
+            {
+                Console.WriteLine($"The spacecraft holds {availableRooms} astronauts.");
+            }
+            else
+            {
+                Console.WriteLine("The spacecraft is too small.");
+            }
+        }
+    }
+}
