2e3b5c27bd8b34bc8d966b00592ef02895ceeff9
Ivaylo Ninov <ivailomn216@gmail.com>
2022-09-30 15:09:31 +0300

    Add Two Task

---
+++ C#-Fundamentals/Basic-Syntax-Conditional-Statements-And-Loops/11.MultiplicationTable2.0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _11.MultiplicationTable2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOne = int.Parse(Console.ReadLine());
+            int numTwo = int.Parse(Console.ReadLine());
+
+            int result = 0;
+            for (int i = numTwo; i <= 10; i++)
+            {
+                result = numOne * i;
+
+                Console.WriteLine($"{numOne} X {i} = {result}");
+            }
+
+            if (numTwo > 10)
+            {
+                result = numOne * numTwo;
+
+                Console.WriteLine($"{numOne} X {numTwo} = {result}");
+            }
+        }
+    }
+}
