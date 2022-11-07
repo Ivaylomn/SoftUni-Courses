41dd8124b89ac52705a9966e86383a6088a6650d
Ivaylo Ninov <ivailomn216@gmail.com>
2022-11-07 18:16:31 +0200

    initial commit

---
+++ C#-Web/HouseRentingSystem/HouseRentingSystem/Extensions/HouseRentingSystemServiceCollectionExtension.cs
@@ -0,0 +1,14 @@
+﻿using HouseRentingSystem.Infrastructure.Data.Common;
+
+namespace HouseRentingSystem.Extensions
+{
+    public static class HouseRentingSystemServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository, Repository>();
+
+            return services;
+        }
+    }
+}
