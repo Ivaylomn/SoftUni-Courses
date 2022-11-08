2ccaac9c841ffef9f80258bf007554c420ff4045
Ivaylo Ninov <ivailomn216@gmail.com>
2022-11-08 17:17:20 +0200

    Create AgentControllere,Models and View

---
+++ C#-Web/HouseRentingSystem/HouseRentingSystem/Controllers/AgentController.cs
@@ -0,0 +1,19 @@
+﻿using HouseRentingSystem.Core.Models.Agent;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Controllers
+{
+    public class AgentController : Controller
+    {
+        public IActionResult Become()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Become(BecomeAgentFormModel becomeAgentModel)
+        {
+            return RedirectToAction(nameof(HouseController.All), "House");
+        }
+    }
+}
