33cc8212419f293d24f7fa53b2cba1c1362635d8
Ivaylo Ninov <ivailomn216@gmail.com>
2022-11-08 11:29:03 +0200

    Added Data Entities for DataBase

---
+++ C#-Web/HouseRentingSystem/HouseRentingSystem.Infrastructure/Data/Entities/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Infrastructure.Data.DataConstants.Category;
+namespace HouseRentingSystem.Infrastructure.Data.Entities
+{
+    public class Category
+    {
+        public Category()
+        {
+            Houses = new List<House>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLenght)]
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<House> Houses { get; set; }
+    }
+}
