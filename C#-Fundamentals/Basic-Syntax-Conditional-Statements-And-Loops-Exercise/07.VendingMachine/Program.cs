131806c7d653307e1153bbc8188743e85d94c8d8
Ivaylo Ninov <ivailomn216@gmail.com>
2022-11-09 10:53:38 +0200

    Initial commit

---
+++ C#-Fundamentals/Basic-Syntax-Conditional-Statements-And-Loops-Exercise/07.VendingMachine/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace _07.VendingMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string coins = Console.ReadLine();
+
+            double sum = 0;
+            while (coins != "Start")
+            {
+                double currentCoins = double.Parse(coins);
+                if (currentCoins == 0.1 || currentCoins == 0.2 
+                    || currentCoins == 0.5 || currentCoins == 1 || currentCoins == 2)
+                {
+                    sum += currentCoins;
+                }
+                else
+                {
+                    Console.WriteLine($"Cannot accept {currentCoins}");
+                }
+
+                coins = Console.ReadLine();
+            }
+
+            string product = Console.ReadLine();
+            double price = 0;
+            while (product != "End")
+            {
+                switch (product)
+                {
+                    case "Nuts":
+                        price = 2.0;
+                        break;
+                    case "Water":
+                        price = 0.7;
+                        break;
+                    case "Crisps":
+                        price = 1.5;
+                        break;
+                    case "Soda":
+                        price = 0.8;
+                        break;
+                    case "Coke":
+                        price = 1.0;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid product");
+                        product = Console.ReadLine();
+                        continue;
+
+                }
+                if (price <= sum)
+                {
+                    sum = sum - price;
+                    Console.WriteLine($"Purchased {product.ToLower()}");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, not enough money");
+                }
+
+                product = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Change: {sum:F2}");
+        }
+    }
+}
---
+++ C#-Fundamentals/Basic-Syntax-Conditional-Statements-And-Loops-Exercise/09.PadawanEquipment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _09.PadawanEquipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double amountMoney = double.Parse(Console.ReadLine());
+            int studentsCount = int.Parse(Console.ReadLine());
+            double lightsabersPrice = double.Parse(Console.ReadLine());
+            double robesPrice = double.Parse(Console.ReadLine());
+            double beltsPrice = double.Parse(Console.ReadLine());
+
+            double studentsDiscount = Math.Ceiling(studentsCount + studentsCount * 0.10);
+            double everySixBeltsFree = studentsCount - studentsCount / 6;
+            double total = lightsabersPrice * studentsDiscount + robesPrice * studentsCount + beltsPrice * everySixBeltsFree;
+
+            if (total <= amountMoney)
+            {
+                Console.WriteLine($"The money is enough - it would cost {total:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"John will need {Math.Abs(amountMoney - total):f2}lv more.");
+            }
+        }
+    }
+}
