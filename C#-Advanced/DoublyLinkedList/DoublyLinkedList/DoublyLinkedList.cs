915ae57ace4ee2b4df40a3229e2ca158348b4e7c
Ivaylo Ninov <ivailomn216@gmail.com>
2023-01-23 15:31:14 +0200

    Added new class DoublyLinkedList

---
+++ C#-Advanced/DoublyLinkedList/DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDoublyLinkedList
+{
+    public class DoublyLinkedList
+    {
+        private ListNode head;
+        private ListNode tail;
+    }
+}
