2e4924a98affde1d3cdd81569f4f571deb0d925c
Ivaylo Ninov <ivailomn216@gmail.com>
2023-01-24 15:38:09 +0200

    Initial commit

--- C#-Advanced/CustomList/CustomList/List.cs
+++ C#-Advanced/CustomList/CustomList/List.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CustomList
+﻿namespace CustomList
 {
-    internal class List
+    public class List
     {
+        private const int InitialCapacity = 2;
+        private int[] items;
     }
 }
