97449b3f8a5cc38ae8a5f5edc06fa9f5d4c7406a
Ivaylo Ninov <ivailomn216@gmail.com>
2023-02-06 15:16:36 +0200

    Initial commit

---
+++ C#-Advanced/Generics-Exercise/GenericSwapMethodString/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericSwapMethodString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Box<string> box = new Box<string>(); ;
+            while (n > 0)
+            {
+                box.Add(Console.ReadLine());
+                n--;
+            }
+
+            int[] index = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            box.Swap(index[0], index[1]);
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
