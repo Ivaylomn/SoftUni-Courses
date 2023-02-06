c4a3f3dfe9471c3778390c62801dce4b518ba3b5
Ivaylo Ninov <ivailomn216@gmail.com>
2023-02-06 14:41:58 +0200

    Initial commit

---
+++ C#-Advanced/Generics-Exercise/GenericBoxOfInteger/Box.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; private set; }
+
+        public override string ToString() 
+            => $"{typeof(T)}: {Value}";
+
+    }
+}
