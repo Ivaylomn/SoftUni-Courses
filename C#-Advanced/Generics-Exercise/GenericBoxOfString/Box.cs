2cb52e282333b9b4a6f6e9b4fe1dd9f4ccc50230
Ivaylo Ninov <ivailomn216@gmail.com>
2023-02-06 14:27:58 +0200

    Initial commit

---
+++ C#-Advanced/Generics-Exercise/GenericBoxOfString/Box.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace GenericBoxOfString
+{
+    public class Box
+    {
+        private List<string> value = new List<string>();
+
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (var item in this.value)
+            {
+                stringBuilder.AppendLine($"{item.GetType()}: {item}");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+
+        public void Add(string value)
+        {
+            this.value.Add(value);
+        }
+    }
+}
---
+++ C#-Advanced/Generics-Exercise/GenericBoxOfString/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Box box = new Box();
+
+            while (n > 0)
+            {
+                string value = Console.ReadLine();
+                
+                box.Add(value);
+                n--;
+            }
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
