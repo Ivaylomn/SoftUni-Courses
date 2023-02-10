fa89eff6a0c3ee5ddf0f6bfdc2d00f158231b971
Ivaylo Ninov <ivailomn216@gmail.com>
2023-02-10 11:44:52 +0200

    Initial commit

--- C#-OOP/Inheritance/SingleInheritance/StartUp.cs
+++ C#-OOP/Inheritance/SingleInheritance/StartUp.cs
@@ -6,9 +6,6 @@ namespace Farm
     {
         static void Main(string[] args)
         {
-                Dog dog = new Dog();
-                dog.Eat();
-                dog.Bark();
         }
     }
 }
