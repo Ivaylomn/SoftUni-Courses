d842a88a981fe15130a4c2a03be26cda4bc9050c
Ivaylo Ninov <ivailomn216@gmail.com>
2023-02-20 11:46:40 +0200

    Initial commit

---
+++ C#-OOP/Encapsulation-Exercise/ShoppingSpree/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+		private string name;
+		private decimal money;
+		private List<Product> bag;
+
+		public Person(string name, decimal money)
+		{
+			Name = name;
+			Money = money;
+			bag = new List<Product>();
+		}
+
+		public string Name
+		{
+			get { return name; }
+			private set 
+			{
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                name = value; 
+			}
+		}
+
+		public decimal Money
+		{
+			get { return money; }
+			private set 
+			{
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                money = value; 
+			}
+		}
+
+		public IReadOnlyCollection<Product> Bag 
+			=> bag.AsReadOnly();
+
+		public void AddProduct(Product product)
+		{
+			Money -= product.Cost;
+			bag.Add(product);
+		}
+	}
+}
