0923cd1e30879cfccdf7e42534a9432fde858571
Ivaylo Ninov <ivailomn216@gmail.com>
2023-02-20 17:52:35 +0200

    Initial commit

---
+++ C#-OOP/InterfacesAndAbstraction/Cars/Contracts/ICar.cs
@@ -0,0 +1,13 @@
+﻿namespace Cars.Contracts
+{
+    public interface ICar
+    {
+        public string Model { get; }
+
+        public string Color { get; }
+
+        string Start();
+
+        string Stop();
+    }
+}
