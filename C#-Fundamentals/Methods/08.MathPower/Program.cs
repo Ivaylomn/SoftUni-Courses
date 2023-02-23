a713de7f59847dfba12caa2920e5f31ea8fb437a
Ivaylo Ninov <ivailomn216@gmail.com>
2023-02-23 17:14:42 +0200

    Initial commit

---
+++ C#-Fundamentals/Methods/08.MathPower/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _08.MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+
+            double rezult = MathPower(number, power);
+
+            Console.WriteLine(rezult);
+        }
+
+        static double MathPower(double number, double power)
+        {
+            return Math.Pow(number, power);
+        }
+    }
+}
---
+++ C#-Fundamentals/Methods/10.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _10.MultiplyEvensByOdds
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            input = Math.Abs(input);
+
+            int oddSum = GetSumOdds(input);
+            int evenSum = GetSumEvens(input);
+
+            Console.WriteLine(MultiplyEO(oddSum, evenSum));
+        }
+
+        static int GetSumEvens(int n)
+        {
+            string inputArray = n.ToString();
+            int sumEven = 0;
+            foreach (var digit in inputArray)
+            {
+                if (digit % 2 == 0)
+                {
+                    sumEven += (int)char.GetNumericValue(digit);
+                }
+            }
+
+            return sumEven;
+        }
+
+        static int GetSumOdds(int n)
+        {
+            string inputArray = n.ToString();
+            int sumOdds = 0;
+            foreach (var digit in inputArray)
+            {
+                if (digit % 2 != 0)
+                {
+                    sumOdds += (int)char.GetNumericValue(digit);
+                }
+            }
+
+            return sumOdds;
+        }
+
+        static int MultiplyEO(int n, int n1)
+        {
+            int result = n * n1;
+            return result;
+        }
+    }
+}
