9e248ec55461861a9ea7e910c46679fecd053fa9
Ivaylo Ninov <ivailomn216@gmail.com>
2023-02-24 23:36:11 +0200

    Initial commit

---
+++ C#-Fundamentals/Methods-Exercise/05.AddAndSubtract/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _05.AddAndSubtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int subtract = Subtract(Sum(firstNumber, secondNumber), thirdNumber);
+
+            Console.WriteLine(subtract);
+        }
+
+        private static int Subtract(int v, int thirdNumber)
+        {
+            return v - thirdNumber;
+        }
+
+        private static int Sum(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
---
+++ C#-Fundamentals/Methods-Exercise/06.MiddleCharacters/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _06.MiddleCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string middle = GetMiddle(text);
+
+            Console.WriteLine(middle);
+        }
+
+        private static string GetMiddle(string text)
+        {
+            if (text.Length % 2 == 0)
+            {
+                return $"{text[text.Length / 2 - 1]}{text[text.Length / 2]}";
+            }
+
+            return $"{text[(text.Length - 1) / 2]}";
+        }
+    }
+}
