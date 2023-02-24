261b517a3a05d7773fa8d1c8bce573ddc44c1c62
Ivaylo Ninov <ivailomn216@gmail.com>
2023-02-24 23:16:36 +0200

    Initial commit

---
+++ C#-Fundamentals/Methods-Exercise/02.VowelsCount/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _02.VowelsCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            int vowelsCount = GetVowelsCharCount(text);
+
+            Console.WriteLine(vowelsCount);
+        }
+
+        private static int GetVowelsCharCount(string text)
+        {
+            text = text.ToLower();
+
+            int counter = 0;
+            foreach (char leter in text)
+            {
+                //A, E, I, O, U
+                if (leter == 'a' || leter == 'e' || leter == 'i' || leter == 'o' || leter == 'u')
+                {
+                    counter++;
+                }
+
+            }
+
+            return counter;
+        }
+    }
+}
