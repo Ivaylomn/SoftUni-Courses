de08c12faa509616b811cc7a199933c77cf385c6
Ivaylo Ninov <ivailomn216@gmail.com>
2023-02-24 23:14:09 +0200

    Initial commit

---
+++ C#-Fundamentals/Methods-Exercise/01.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _01.SmallestOfThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOne = int.Parse(Console.ReadLine());
+            int numTwo = int.Parse(Console.ReadLine());
+            int numThree = int.Parse(Console.ReadLine());
+
+            int smallNumber = GetSmalestNumber(numOne, numTwo, numThree);
+
+            Console.WriteLine(smallNumber);
+        }
+
+        static int GetSmalestNumber(int one, int two, int three)
+        {
+            int smallNumber = Math.Min(Math.Min(one, two), three);
+
+            return smallNumber;
+        }
+    }
+}
