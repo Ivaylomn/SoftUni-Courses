6bdb07726c86c7cec49e7eef0570357f19983614
Ivaylo Ninov <ivailomn216@gmail.com>
2023-02-24 23:19:56 +0200

    Initial commit

---
+++ C#-Fundamentals/Methods-Exercise/03.CharactersInRange/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _03.CharactersInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+
+            char start = firstChar;
+            char end = secondChar;
+
+            if (secondChar < firstChar)
+            {
+                start = secondChar;
+                end = firstChar;
+            }
+
+            PrintCharacketrInRange(start, end);
+        }
+
+        private static void PrintCharacketrInRange(char start, char end)
+        {
+            for (char i = (char)(start + 1); i < end; i++)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ C#-Fundamentals/Methods-Exercise/04.PasswordValidator/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace _04.PasswordValidator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            bool isValidPassword = true;
+
+            if (!PasswordLenght(password))
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+                isValidPassword = false;
+            }
+
+            if (ConsistOfLettersAndDigits(password))
+            {
+                Console.WriteLine("Password must consist only of letters and digits");
+                isValidPassword = false;
+            }
+
+            if (!ContainsDigitCount(password, 2))
+            {
+                Console.WriteLine("Password must have at least 2 digits");
+                isValidPassword = false;
+            }
+
+            if (isValidPassword)
+            {
+                Console.WriteLine("Password is valid");
+            }
+        }
+
+        private static bool ContainsDigitCount(string password, int count)
+        {
+            int digitCount = 0;
+
+            foreach (char symbol in password)
+            {
+                if (char.IsDigit(symbol))
+                {
+                    digitCount++;
+
+                    if (digitCount == count)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private static bool ConsistOfLettersAndDigits(string password)
+        {
+            foreach (char symbol in password)
+            {
+                if (!char.IsLetterOrDigit(symbol))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool PasswordLenght(string password)
+        {
+            return password.Length >= 6 && password.Length <= 10;
+        }
+    }
+}
