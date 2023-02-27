2076f32a5533ca1e26479c3ff70ac2865a3a63e8
Ivaylo Ninov <ivailomn216@gmail.com>
2023-02-27 16:19:08 +0200

    Initial commit

---
+++ C#-Fundamentals/Lists/06.ListManipulationBasics/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.ListManipulationBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "end")
+                {
+                    break;
+                }
+
+                string[] tokens = command.Split();
+
+                switch (tokens[0])
+                {
+                    case "Add":
+                        numbers.Add(int.Parse(tokens[1]));
+                        break;
+                    case "Remove":
+                        numbers.Remove(int.Parse(tokens[1]));
+                        break;
+                    case "RemoveAt":
+                        numbers.RemoveAt(int.Parse(tokens[1]));
+                        break;
+                    case "Insert":
+                        int numbersToInsert = int.Parse(tokens[1]);
+                        int indexToInsert = int.Parse(tokens[2]);
+                        numbers.Insert(indexToInsert, numbersToInsert);
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
