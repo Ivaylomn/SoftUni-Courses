2605b5fef96ab23fda614b0511ffbe5c0089dfbb
Ivaylo Ninov <ivailomn216@gmail.com>
2023-02-28 14:35:44 +0200

    Initial commit

--- C#-OOP/Polymorphism/Shapes/StartUp.cs
+++ C#-OOP/Polymorphism/Shapes/StartUp.cs
@@ -6,10 +6,6 @@ namespace Shapes
     {
         static void Main(string[] args)
         {
-            Rectangle rectangle = new Rectangle(3, 6);
-            Circle circle = new Circle(5);
-            Console.WriteLine(rectangle.Draw());
-            Console.WriteLine(circle.Draw());
         }
     }
 }
