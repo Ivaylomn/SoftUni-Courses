aa8ef63ab58b4ce74da96ed7fb5f461b9a041c1c
Ivaylo Ninov <ivailomn216@gmail.com>
2023-02-28 14:33:49 +0200

    Initial commit

---
+++ C#-OOP/Polymorphism/Shapes/Rectangle.cs
@@ -0,0 +1,40 @@
+﻿namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double width;
+        private double height;
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double Height
+        {
+            get { return height; }
+            private set { height = value; }
+        }
+
+        public double Width
+        {
+            get { return width; }
+            private set { width = value; }
+        }
+
+
+        public override double CalculateArea()
+        {
+            return Height * Width;
+        }
+
+        public override double CalculatePerimeter()
+            => (Height + Width) * 2;
+
+        public override string Draw()
+        {
+            return $"{base.Draw()}{this.GetType().Name}";
+        }
+    }
+}
