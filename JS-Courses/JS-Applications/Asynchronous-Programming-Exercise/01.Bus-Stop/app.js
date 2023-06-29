dba4b80472f258fc4e895da0087c0eee6fcfeec9
Ivaylo Ninov <ivailomn216@gmail.com>
2023-06-29 11:27:30 +0300

    Initial commit

--- JS-Courses/JS-Applications/Asynchronous-Programming-Exercise/01.Bus-Stop/app.js
+++ JS-Courses/JS-Applications/Asynchronous-Programming-Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,25 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const stopId = document.getElementById('stopId').value;
+    const stopName = document.getElementById('stopName');
+    const busesList = document.getElementById('buses');
+    
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+    try {
+        busesList.innerHTML = '';
+        
+        const res = await fetch(url);
+        if (res.status != 200) {
+            throw new Error('Error');
+        }
+        const data = await res.json();
+        stopName.textContent = data.name
+
+        for (const [stop, time] of Object.entries(data.buses)) {
+            const liElement = document.createElement('li');
+            liElement.textContent = `Bus ${stop} arrives in ${time} minutes`;
+            busesList.appendChild(liElement);
+        }
+    } catch (error) {
+        stopName.textContent = error.message;
+    }
 }
