fe02cab773394023585af206a2646bb82727ddc0
Ivaylo Ninov <ivailomn216@gmail.com>
2023-06-29 12:00:57 +0300

    Initial commit

--- JS-Courses/JS-Applications/Asynchronous-Programming-Exercise/02.Bus-Schedule/app.js
+++ JS-Courses/JS-Applications/Asynchronous-Programming-Exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,34 @@
 function solve() {
+    const infoBox = document.querySelector('#info span');
+    const departButton = document.getElementById('depart');
+    const arriveButton = document.getElementById('arrive');
 
-    function depart() {
-        console.log('Depart TODO...');
+    let initialStop = 'depot';
+    let currentBusStop = '';
+    const baseUrl = `http://localhost:3030/jsonstore/bus/schedule/`;
+    async function depart() {
+        try {
+            const res = await fetch(`${baseUrl}${initialStop}`);
+            const data = await res.json();
+
+            infoBox.textContent = `Next stop ${data.name}`;
+            currentBusStop = data.name;
+            initialStop = data.next;
+
+            departButton.disabled = true;
+            arriveButton.disabled = false;
+        } catch (error) {
+            departButton.disabled = true;
+            arriveButton.disabled = true;
+            infoBox.textContent = 'Error';
+        }
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        arriveButton.disabled = true;
+        departButton.disabled = false;
+
+        infoBox.textContent = `Arriving ${currentBusStop}`;
     }
 
     return {
