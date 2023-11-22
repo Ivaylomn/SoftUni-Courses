5895c33e1b7af1877330679e60c716c270211593
Ivaylo <ivailomn216@gmail.com>
2023-11-22 13:27:34 +0200

    Add new project

---
+++ C#-Courses/C#-Fundamentals/ObjectsAndClasses/ConsoleApp1/Song.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleApp1
+{
+    public class Song
+    {
+        private string name;
+        private string time;
+        private string type;
+
+        public Song(string name, string time, string type)
+        {
+            Name = name;
+            Time = time;
+            Type = type;
+        }
+
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+        public string Time
+        {
+            get => time;
+            private set
+            {
+                time = value;
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+            private set
+            {
+                type = value;
+            }
+        }
+    }
+}
