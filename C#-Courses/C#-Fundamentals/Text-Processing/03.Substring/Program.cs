f447d487a8464fc6fdce13aa638a4d735de10603
Ivaylo <ivailomn216@gmail.com>
2023-11-25 16:18:04 +0200

    Initial commit

---
+++ C#-Courses/C#-Fundamentals/Text-Processing/03.Substring/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _03.Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            int findIndex = text.IndexOf(pattern);
+
+            while (findIndex != -1) 
+            {
+                text = text.Remove(findIndex, pattern.Length);
+                findIndex = text.IndexOf(pattern);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ C#-Courses/C#-Fundamentals/Text-Processing/05.DigitsLettersAndOther/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _05.DigitsLettersAndOther
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string digit = "";
+            string letter = "";
+            string other = "";
+            foreach (var item in input)
+            {
+                if (char.IsDigit(item))
+                {
+                    digit += item;
+                }
+                else if (char.IsLetter(item)) 
+                {
+                    letter += item;
+                }
+                else
+                {
+                    other += item;
+                }
+            }
+
+            Console.WriteLine(digit);
+            Console.WriteLine(leter);
+            Console.WriteLine(other);
+        }
+    }
+}
