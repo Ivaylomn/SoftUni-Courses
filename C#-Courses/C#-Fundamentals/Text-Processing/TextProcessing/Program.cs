719db27455cdf69b10f91c1970b86b6c59942482
Ivaylo <ivailomn216@gmail.com>
2023-11-25 14:35:37 +0200

    Initial commit

---
+++ C#-Courses/C#-Fundamentals/Text-Processing/TextProcessing/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace TextProcessing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "end") 
+            {
+                //Reversed with LINQ
+                //string reversedWord = new string (input.Reverse().ToArray());
+
+                string reversedWord = string.Empty;
+
+                for (int i = input.Length - 1; i >= 0; i--)
+                {
+                    reversedWord += input[i];
+                }
+
+                Console.WriteLine($"{input} = {reversedWord}");
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
