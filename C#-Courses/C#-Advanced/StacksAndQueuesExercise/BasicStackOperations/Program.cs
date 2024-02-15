062b21c790c6d06f3def261e2631203015fc8185
Ivaylo Ninov <ivailomn216@gmail.com>
2024-02-15 18:11:36 +0200

    initial commit

---
+++ C#-Courses/C#-Advanced/StacksAndQueuesExercise/BasicStackOperations/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace BasicStackOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> stack = new Stack<int>(numbers);
+
+            for (int i = 0; i < input[1]; i++)
+            {
+                stack.Pop();
+            }
+
+            bool isContains = true;
+            if (stack.Contains(input[2]))
+            {
+                Console.WriteLine("true");
+                isContains = false;
+            }
+
+            if (!stack.Contains(input[2]))
+            {
+                if (stack.Count > 0)
+                {
+                    Console.WriteLine(stack.Min());
+                    isContains = false;
+                }
+            }
+
+            if (isContains)
+            {
+                Console.WriteLine(stack.Count());
+            }
+        }
+    }
+}
