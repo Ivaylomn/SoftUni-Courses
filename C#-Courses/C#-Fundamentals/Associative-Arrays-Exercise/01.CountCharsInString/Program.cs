42b9d15985884b01658c6a9405294baff03c8601
Ivaylo Ninov <ivailomn216@gmail.com>
2024-02-19 15:17:16 +0200

    initial commit

---
+++ C#-Courses/C#-Fundamentals/Associative-Arrays-Exercise/01.CountCharsInString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.CountCharsinString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    continue;
+                }
+                if (dict.ContainsKey(text[i]))
+                {
+                    dict[text[i]]++;
+                }
+                else
+                {
+                    dict.Add(text[i], 1);
+                }
+            }
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
