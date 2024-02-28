c1d1ba907a41d065c3190e3c70265a8ba4bad3c1
Ivaylo Ninov <ivailomn216@gmail.com>
2024-02-28 16:54:46 +0200

    Add new project

---
+++ C#-Courses/C#-Fundamentals/Associative-Arrays-Exercise/02.MinerTask/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _02.MinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            string resource = Console.ReadLine();
+
+            while (resource != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (dict.ContainsKey(resource))
+                {
+                    dict[resource] += quantity;
+                }
+                else
+                {
+                    dict.Add(resource, quantity);
+                }
+
+                resource = Console.ReadLine();
+            }
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
